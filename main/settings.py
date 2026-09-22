637af40bd9ab5bf203cacc360f6e750e77f5e52a
Alexander <129003161+MrShrimp5566@users.noreply.github.com>
2026-09-22 12:36:53 -0500

    feat: Add throttling configuration for REST framework to limit user requests

--- main/settings.py
+++ main/settings.py
@@ -125,4 +125,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "user": "120/minute",
+    },
 }
